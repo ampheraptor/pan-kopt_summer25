ff43696f871255e7e25946026347a453d7fe1ddd
javrith <ihave3catslol@gmail.com>
2025-06-01 12:46:33 -0700

    Revert "Reapply "Merge branch 'main' of https://github.com/ampheraptor/pan-kopt_summer25""
    This reverts commit 06d96d4777f3327bee1c80e3fee7b26a795e4a28.

--- Assets/Scripts/MinesweeperProto/ProtoTraverser.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ProtoTraverser : MonoBehaviour
-{
-    //Doesnt do anything yet, will move with WASD by grid increments, wont be able to walk past edge of grid or "covered" cells
-}
