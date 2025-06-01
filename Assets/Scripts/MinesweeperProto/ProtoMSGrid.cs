5fdc06a9dc3f3044d6b8b365233e3a7f3556d604
javrith <ihave3catslol@gmail.com>
2025-06-01 12:55:11 -0700

    Fixing proto grid
    Fixing proto grid

--- Assets/Scripts/MinesweeperProto/ProtoMSGrid.cs
+++ Assets/Scripts/MinesweeperProto/ProtoMSGrid.cs
@@ -5,35 +5,33 @@ using UnityEngine;
 
 public class ProtoMSGrid : Singleton<ProtoMSGrid>
 {
-    [SerializeField] public static readonly int ROWS = 10;
-    [SerializeField] public static readonly int COLS = 10;
+    //These are constants but need to just be private so they can be exposed in the editor
+    [SerializeField] protected int ROWS = 10;
+    [SerializeField] protected int COLS = 10;
 
     private float cellWidth;
     private float cellHeight;
 
-    private ProtoMSCell[,] grid = new ProtoMSCell[ROWS, COLS];
+    protected ProtoMSCell[,] grid;
     [SerializeField] private GameObject cellPrefab;
 
 
     public int minesRandomMin;
     public int minesRandomMax;
     private int totalMines;
-    private int totalSafeCells;
-    private int cellsRevealed = 0;
 
-    public void StartGrid()
+    private void Start()
     {
+        Initialize();
+    }
+
+    protected void Initialize()
+    {
+        grid = new ProtoMSCell[ROWS, COLS];
         cellWidth = cellPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
         cellHeight = cellPrefab.GetComponent<SpriteRenderer>().bounds.size.y;
         CreateGrid();
         CenterMyself();
-        Debug.Log("started grid");
-    }
-
-    public void RestartGrid()
-    {
-        EraseGrid();
-        CreateGrid();
     }
     private void CreateGrid()
     {
@@ -41,12 +39,12 @@ public class ProtoMSGrid : Singleton<ProtoMSGrid>
         {
             for (int x = 0; x < ROWS; x++)
             {
-                //Make a new cell 
-                ProtoMSCell cell = Instantiate(cellPrefab, transform).GetComponent<ProtoMSCell>();
+                //Make a new cell
+                GameObject cell = Instantiate(cellPrefab, transform);
                 //Put it in the 2D Array
-                grid[x, y] = cell;
+                grid[x, y] = cell.GetComponent<ProtoMSCell>();
                 //Give cell it's 2D coords
-                grid[x, y].SetXY(x, y, this);
+                grid[x, y].SetXY(x, y);
                 //Place it correctly
                 float newX = x * cellWidth;
                 float newY = y * cellHeight;
@@ -62,7 +60,8 @@ public class ProtoMSGrid : Singleton<ProtoMSGrid>
             {
                 RandomEmptyCell().SetMine(true);
             }
-        } else
+        }
+        else
         {
             Debug.Log("ERROR in ProtoMSGrid.cs: mine count exceeds cell count");
         }
@@ -74,11 +73,9 @@ public class ProtoMSGrid : Singleton<ProtoMSGrid>
                 grid[x, y].CountNeighborMines(); // needs to be in its own loop because otherwise it will try to count cells that dont exist yet
             }
         }
-        totalSafeCells = COLS * ROWS - totalMines;
-        cellsRevealed = 0;
     }
 
-    private ProtoMSCell RandomEmptyCell()
+    protected ProtoMSCell RandomEmptyCell()
     {
         ProtoMSCell pms = grid[Random.Range(0, ROWS), Random.Range(0, COLS)];
         if (pms.GetMine())
@@ -90,14 +87,6 @@ public class ProtoMSGrid : Singleton<ProtoMSGrid>
         return pms;
     }
 
-    public void ReportRevealed()
-    {
-        cellsRevealed++;
-        if (cellsRevealed == totalSafeCells)
-        {
-            FindFirstObjectByType<RoundHandler>().ReportVictory();
-        }
-    }
 
     private void CenterMyself()
     {
@@ -111,6 +100,63 @@ public class ProtoMSGrid : Singleton<ProtoMSGrid>
         transform.position = new Vector3(newX, newY, 0);
 
     }
+
+    public ProtoMSCell GetCell(int x, int y)
+    {
+        //Debug.Log("I'm getting Cell: " + x + ", " + y + ". ");
+        return grid[x, y];
+    }
+
+    public int GetGridRows()
+    {
+        return ROWS;
+    }
+
+    public int GetGridCols()
+    {
+        return COLS;
+    }
+
+
+
+    //Called from Input system
+    public void ToggleDebugNumbers(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            ShowNumbers();
+        }
+        else if (context.canceled)
+        {
+            HideNumbers();
+        }
+    }
+
+    private void ShowNumbers()
+    {
+        foreach (ProtoMSCell c in grid)
+        {
+            c.ShowNumber(true);
+        }
+    }
+
+    private void HideNumbers()
+    {
+        foreach (ProtoMSCell c in grid)
+        {
+            c.ShowNumber(false);
+        }
+    }
+
+    public void ReportRevealed()
+    {
+        cellsRevealed++;
+        if (cellsRevealed == totalSafeCells)
+        {
+            FindFirstObjectByType<RoundHandler>().ReportVictory();
+        }
+    }
+
 
     public ProtoMSCell GetCell(int x, int y)
     {
